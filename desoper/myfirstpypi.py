130d326ed2a4311d9565272e092188cd29865c41
miguelcifuentes <45666594+miguelcifuentes@users.noreply.github.com>
2022-09-28 16:51:48 -0500

    Update myfirstpypi.py

--- desoper/myfirstpypi.py
+++ desoper/myfirstpypi.py
@@ -2,19 +2,19 @@
 r'''
 myfirstpypi 
 '''
-
-
-def hello():
-    r'''
-    Hello function
-    '''
-    return 'Hello, World!'
-
-def lola(x):
-    r'''
-    lola function
-    '''
-    return x*x
+import dask.array as da
+import pandas as pd
+from multiprocessing import Pool
+from multiprocessing import cpu_count
+import os
+import numpy as np
+import itertools
+import sys
+from anomalies import anomaly
+import numpy as np
+import time
+import warnings
+warnings.filterwarnings("ignore")
 
 if __name__ == '__main__':
     r'''

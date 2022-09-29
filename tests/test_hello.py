591e5b5adad1b49cb05c8dd768683e1377179ed2
miguelcifuentes <45666594+miguelcifuentes@users.noreply.github.com>
2022-09-28 22:42:40 -0500

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -6,11 +6,11 @@ import pandas as pd
 
 class Test_hello(unittest.TestCase):
     def test__working1(self):
-        ls1=myfirstpypi.principal(5,9,0,30,400000).to_numpy()
+        ls1=pd.DataFrame(myfirstpypi.principal(5,9,0,30,400000))
         self.assertEqual(12,ls1.shape[0], True)
 
     def test__working2(self):
-        ls2=myfirstpypi.principal(6,9,0,30,400000).to_numpy()
+        ls2=pd.DataFrame(myfirstpypi.principal(6,9,0,30,400000))
         self.assertEqual(142,ls2.shape[0], True)
        
 

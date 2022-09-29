2f32dd6e09c940ffe31373bba96abfb05da5da95
miguelcifuentes <45666594+miguelcifuentes@users.noreply.github.com>
2022-09-28 21:22:52 -0500

    Update test_hello.py

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -6,16 +6,16 @@ import pandas as pd
 
 class Test_hello(unittest.TestCase):
     def test__working(self):
-        ls1=pd.DataFrame(myfirstpypi.principal(5,9,0,30,400000))
+        ls1=myfirstpypi.principal(5,9,0,30,400000).z.to_numpy()
         self.assertEqual(12,ls1.shape[0], True)
  
 class Test_hello(unittest.TestCase):
     def test__working(self):
-        ls2=myfirstpypi.principal(6,9,0,30,400000)
+        ls2=myfirstpypi.principal(6,9,0,30,400000).z.to_numpy()
         self.assertEqual(142,ls2.shape[0], True)
        
 
 
 if __name__ == '__main__':
     unittest.main()
-#n estudiaa esta parte,clas test evalua la funcion
+
